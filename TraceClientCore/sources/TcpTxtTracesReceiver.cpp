fb76bef58f827c9a038446adc7939ebf4784a360
Danny Thibaudeau <cobreti_@hotmail.com>
2014-08-29 11:13:40 -0400

    load js content from file

--- TraceClientCore/sources/TcpTxtTracesReceiver.cpp
+++ TraceClientCore/sources/TcpTxtTracesReceiver.cpp
@@ -18,54 +18,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <QFile>
+#include <QTextStream>
+
 namespace TraceClientCore
 {
-	//char* base64( const unsigned char *input, int length )
-	//{
-	//	// bio is simply a class that wraps BIO* and it free the BIO in the destructor.
-
-	//	BIO* b64(BIO_f_base64()); // create BIO to perform base64
-	//	BIO_set_flags(b64,BIO_FLAGS_BASE64_NO_NL);
-
-	//	BIO mem(BIO_s_mem()); // create BIO that holds the result
-
-	//	// chain base64 with mem, so writing to b64 will encode base64 and write to mem.
-	//	BIO_push(b64, mem);
-
-	//	// write data
-	//	bool done = false;
-	//	int res = 0;
-	//	while(!done)
-	//	{
-	//		res = BIO_write(b64, data.data, (int)data.size);
-
-	//		if(res <= 0) // if failed
-	//		{
-	//			if(BIO_should_retry(b64)){
-	//				continue;
-	//			}
-	//			else // encoding failed
-	//			{
-	//				/* Handle Error!!! */
-	//			}
-	//		}
-	//		else // success!
-	//			done = true;
-	//	}
-
-	//	BIO_flush(b64);
-
-	//	// get a pointer to mem's data
-	//	char* dt;
-	//	long len = BIO_get_mem_data(mem, &dt);
-
-	//	// assign data to output
-	//	std::string s(dt, len);
-
-	//	return buffer(s.length()+sizeof(char), (byte*)s.c_str());
-	//}
-
-
 	char *base64(const unsigned char *input, int length)
 	{
 	  BIO *bmem, *b64;
@@ -108,29 +65,6 @@ namespace TraceClientCore
 		BIO_free_all(b64);
 
 		return buff;
-
-		// assign data to output
-		//std::string s(dt, len);
-
-		//return buffer(s.length()+sizeof(char), (byte*)s.c_str());
-
-		//BIO_write(b64, input, length);
-	  //BIO_flush(b64);
-	  //BIO_get_mem_ptr(b64, &bptr);
-
-	  //size_t l = bptr->length;
-
-	  //char *buff = (char *)malloc(bptr->length);
-
-   //   if ( !(bptr->length > 0) )
-   //       return NULL;
-
-	  //memcpy(buff, bptr->data, bptr->length-1);
-	  //buff[bptr->length-1] = 0;
-
-	  //BIO_free_all(b64);
-
-	  //return buff;
 	}
 
 	char *decode64(unsigned char *input, int length)
@@ -348,40 +282,25 @@ namespace TraceClientCore
         char    szCode[16000];
         char    szPort[20];
     
-        sprintf(szPort, "%i", m_pServer->Settings().PortNumber());
-        
-        strcpy(szCode, "if ( window.$Nyx == undefined ) {\r\n");
-        strcat(szCode, "   $Nyx = {\r\n");
-        strcat(szCode, "       msgQueue: [],\r\n");
-        strcat(szCode, "       Trace: function(module, text) {\r\n");
-        strcat(szCode, "           $Nyx.msgQueue.push(module + '/' + text + '\\0');\r\n");
-        strcat(szCode, "           if ( $Nyx.msgQueue.length == 1 ) {\r\n");
-        strcat(szCode, "               setTimeout( $Nyx.processMsg, 1 );\r\n");
-        strcat(szCode, "           }\r\n");
-        strcat(szCode, "       },\r\n");
-        strcat(szCode, "       processMsg: function() {\r\n");
-        strcat(szCode, "           var msg = $Nyx.msgQueue.shift();\r\n");
-        strcat(szCode, "           if ( $Nyx.connection != undefined && $Nyx.connection.readyState == 1 )\r\n");
-        strcat(szCode, "               $Nyx.connection.send(msg);\r\n");
-        strcat(szCode, "           if ( $Nyx.msgQueue.length > 0 ) {\r\n");
-        strcat(szCode, "               setTimeout( $Nyx.processMsg, 1 );\r\n");
-        strcat(szCode, "           }\r\n");
-        strcat(szCode, "       }\r\n");
-        strcat(szCode, "   };\r\n");
-        
-        if ( m_pServer->GetUseSSL() )
-            strcat(szCode, "   $Nyx.connection = new WebSocket('wss://localhost:");
-        else
-            strcat(szCode, "   $Nyx.connection = new WebSocket('ws://localhost:");
+        QFile   file("webContent.html");
+        QString content;
 
-        NYXTRACE(0x0, L"code len : " << Nyx::CTF_Int(strlen(szCode)));
-                
-        strcat(szCode, szPort);
-        strcat(szCode, "');\r\n");
-        strcat(szCode, "   $Nyx.connection.onclose = function() { delete $Nyx.connection; }\r\n");
-        strcat(szCode, "}\r\n");
-                
-        rConnHttpHandler.Write( (char*)"text/javascript", szCode, strlen(szCode));
+        if ( file.exists() )
+        {
+            file.open(QIODevice::Text | QIODevice::ReadOnly);
+
+            QTextStream in(&file);
+            content = in.readAll();
+
+            file.close();
+        }
+        else
+        {
+            file.open(QIODevice::Text | QIODevice::WriteOnly);
+            file.close();
+        }
+
+        rConnHttpHandler.Write( (char*)"text/javascript", content.toStdString().c_str(), content.length() );
     }
 
     
